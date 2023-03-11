2ddacbe68da55bacf9f7612371d79860937f9607
R0uter <lomyr.jo@gmail.com>
2023-03-11 13:40:08 +1000

    first commit

---
+++ src/gunicorn.conf.py
@@ -0,0 +1,5 @@
+bind = '0.0.0.0:5001'
+workers = 3
+worker_class = 'gevent'
+worker_connections = 1000
+timeout = 60
---
+++ src/price_models.py
@@ -0,0 +1,22 @@
+openAIModels = {
+    'gpt-3.5-turbo': {
+        'oneDollarToken': 1 / 0.002 * 1000,
+        'name': 'gpt-3.5-turbo-0301',
+    },
+    'davinci': {
+        'oneDollarToken': 1 / 0.02 * 1000,
+        'name': 'text-davinci-003',
+    },
+    'curie': {
+        'oneDollarToken': 1 / 0.002 * 1000,
+        'name': 'text-curie-001',
+    },
+    'babbage': {
+        'oneDollarToken': 1 / 0.0005 * 1000,
+        'name': 'text-babbage-001',
+    },
+    'ada': {
+        'oneDollarToken': 1 / 0.0004 * 1000,
+        'name': 'text-ada-001',
+    }
+}
