f902f6d9aca3d5f923d4bed3d6303ab73358be80
R0uter <lomyr.jo@gmail.com>
2023-03-19 17:51:26 +1000

    make app working

--- scripts/check_log.sh
+++ scripts/check_log.sh
@@ -1,3 +1,3 @@
 ssh ubuntu@3.215.107.112 << EOF
-sudo tail -n 100 /var/log/supervisor/api-server-stdout---supervisor-*.log
+sudo tail -n 100 /var/log/supervisor/gptdock-stdout---supervisor-*.log
 EOF
